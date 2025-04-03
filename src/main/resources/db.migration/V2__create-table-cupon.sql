1035cbc2e48c73cd332d10bafaa5214785ca4b84
heenndrix <hendrixoliveira89@gmail.com>
2025-04-03 16:35:08 -0300

    Feat: creating tables

---
+++ src/main/resources/db.migration/V2__create-table-cupon.sql
@@ -0,0 +1,10 @@
+CREATE TABLE cupon(
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    code VARCHAR(100) NOT NULL,
+    discount INTEGER NOT NULL,
+    valid TIMESTAMP NOT NULL,
+    event_id UUID,
+    FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
+
+
+)
